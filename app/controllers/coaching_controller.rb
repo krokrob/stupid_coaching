516a3455485561196e933206bb24a7bbae159fad
kevin robert <kevin.j.robert@gmail.com>
2015-02-09 13:57:44 +0100

    finished stupid coaching

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,6 +1,13 @@
 class CoachingController < ApplicationController
   def answer
     @query = params[:query];
+    if @query.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @query == "I am going to work right now!"
+      @answer = ""
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 
   def ask
